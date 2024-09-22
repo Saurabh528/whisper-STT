771b83f2a5558ee6f647a84909d0d42526cac926
Saurabh528 <48671234+Saurabh528@users.noreply.github.com>
2024-09-22 23:09:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,23 +10,24 @@ wav_audio_data = st_audiorec()
 
 
 if wav_audio_data:
-    file_path = 'save_recorded_audio.wav'
+    #file_path = 'save_recorded_audio.wav'
     
     # Save the audio file as .wav
-    with open(file_path, "wb") as f:
-        f.write(wav_audio_data)
+    # with open(file_path, "wb") as f:
+    #     f.write(wav_audio_data)
     
-    st.success(f"WAV file saved successfully as {file_path}")
+    #st.success(f"WAV file saved successfully as {file_path}")
+    model = whisper.load_model("tiny")
+
+    start = time.time()
+    result = model.transcribe(wav_audio_data)
+    end = time.time()
+    st.write(end-start)
+    
+    st.write(result['text'])
 
 
 
 
 # Load the 'tiny' model to ensure compatibility with Streamlit Cloud
-model = whisper.load_model("tiny")
 
-start = time.time()
-result = model.transcribe("save_recorded_audio.wav")
-end = time.time()
-st.write(end-start)
-
-st.write(result['text'])
